2ec14a5a63f945b7fdc535a92ae46ba57b8aaa5e
Felipe Jun Fujioka Shida <felipefujioka@Felipes-MacBook-Air.local>
2016-05-13 18:09:14 -0300

    installing components inside the container image

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
-docker build -t app1 ./app1/
-docker build -t app2 ./app2/
-docker build -t app2 ./proxyServer/
+# docker build -t app1 ./app1/
+# docker build -t app2 ./app2/
+# docker build -t app2 ./proxyServer/
 cd proxy
+docker-compose down
+docker-compose build
 docker-compose up -d
+docker-compose ps
 cd ..
