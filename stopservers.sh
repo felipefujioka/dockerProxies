dda8bf24501cff906823a5b6b7d2d1703e77ff2c
Felipe Jun Fujioka Shida <felipefujioka@Felipes-MacBook-Air.local>
2016-05-13 16:41:15 -0300

    composing serving app 1 and 2 and a proxy

--- stopservers.sh
+++ stopservers.sh
@@ -1,4 +1,6 @@
 docker stop app1
 docker stop app2
+docker stop proxyServer
 docker rm app1
 docker rm app2
+docker rm proxyServer
--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 docker build -t app1 ./app1/
 docker build -t app2 ./app2/
+docker build -t app2 ./proxyServer/
 cd proxy
-docker-compose up
+docker-compose up -d
 cd ..
